afe515479d66c18a6c50bbe91bc04762f4205f48
JinsoRaj <jinso.trv18it032@gecbh.ac.in>
2021-09-17 20:19:16 +0530

    await

--- api/index.js
+++ api/index.js
@@ -1,43 +1,32 @@
 const cheerio = require('cheerio');
-const request = require('request');
+const axios = require("axios")
 const express = require('express');
 const app = express();
-const list=[];
+
 app.use(express.json());
 
-function getDetails(provider, id){
-    const url = `https://m.aftership.com/${provider}/${id}`;
-    request({
-        method: 'GET',
-        url: url
-    }, (err, res, body) => {
-
-        if (err){
-            return console.error(err);
-        }
-
-        let $ = cheerio.load(body);
-
-        $('.sc-5t1owe-0').find('li').each(function() {
-            var date = $(this).find('div > div > p:first-child').text();
-            var time = $(this).find('div > div > p:nth-child(2)').text();
-            var status = $(this).find('div:last-child > p > span:first-child').text();
-            var place = $(this).find('div:nth-child(3) > p:nth-child(2)').text();
-            var items = {time, date, status, place}
-            list.push(items);
-        });
-    //return list;
+async function getDetails(url){
+    let list = [];
+    const body = await axios.get(url);
+    let $ = cheerio.load(body.data);
+    $('.sc-5t1owe-0').find('li').each(function () {
+        var date = $(this).find('div > div > p:first-child').text();
+        var time = $(this).find('div > div > p:nth-child(2)').text();
+        var status = $(this).find('div:last-child > p > span:first-child').text();
+        var place = $(this).find('div:nth-child(3) > p:nth-child(2)').text();
+        var items = { time, date, status, place };
+        list.push(items);
     });
+    return list;  
 }
 
 
-app.get('/:provider/:id', (req, res) => {
+app.get('/:provider/:id', async(req, res) => {
     
     try{
-        
-        getDetails(req.params.provider, req.params.id)
-        res.send(list);
-        list.length =0;
+        var turl = `https://m.aftership.com/${req.params.provider}/${req.params.id}`;
+        var result = await getDetails(turl);   
+        res.send(result);
     }
     catch(err){
         console.log(err)
