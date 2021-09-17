13b0a10d9bf4233e6ed0adee0e41f49211d74a4d
JinsoRaj <jinso.trv18it032@gecbh.ac.in>
2021-09-17 15:48:08 +0530

    Vercel  API

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
 # Aftership-Tracking
+Indian courier tracker api + telegram tracker bot
+
+## Request
+```http
+GET localhost:3000/{provider}/{trackingid}/
+```
+#### Currently supported courier services
+
+
+|    {```provider```}     |    ?    |
+|    :----------------    | :-----: |
+| ```ekart```             |   ✅    |
+| ```dtdc```              |   ✅    |
+| ```india-post```        |   ✅    |
+| ```delhivery```         |   🤷‍♀️    |
+| ```bluedart```          |   🤷‍♀️    |
+| ```xpressbees```        |   🤷‍♀️    |
+| ```dhl```               |   🤷‍♀️    |
+| ```ecom```              |   🤷‍♀️    |
+| ```gati```              |   🤷‍♀️    |
+| ```shadowfax```         |   🤷‍♀️    |
+
+## Response
+
+```
+    {
+        time:   "13:04"
+        date:   "Sep 10, 2021"
+        status: "Successfully Delivered"
+        place:  "Trivandrum branch"
+    },
+    {
+        time:   "08:55"
+        date:   "Sep 10, 2021"
+        status: "Out for Delivery"
+        place:  "Trivandrum branch"
+    },
+    {
+        time:   "06:43"
+        date:   "Sep 10, 2021"
+        status: "Received at Facility"
+        place:  "Cochin apex"
+    },
+    {...}...
+    
+```
+## Demo URL
+```
+https://track.aftership.now.sh/{provider}/{trackingid}
+```
+### TODO
+- Telegram bot with tracking notification
---
+++ api/index.js
@@ -0,0 +1,56 @@
+const cheerio = require('cheerio');
+const request = require('request');
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+const list=[];
+
+
+async function getDetails(provider, id){
+    const url = `https://m.aftership.com/${provider}/${id}`;
+    request({
+        method: 'GET',
+        url: url
+    }, (err, res, body) => {
+
+        if (err){
+            return console.error(err);
+        }
+
+        let $ = cheerio.load(body);
+        list.length =0;
+
+        $('.sc-5t1owe-0').find('li').each(function() {
+            var date = $(this).find('div > div > p:first-child').text();
+            var time = $(this).find('div > div > p:nth-child(2)').text();
+            var status = $(this).find('div:last-child > p > span:first-child').text();
+            var place = $(this).find('div:nth-child(3) > p:nth-child(2)').text();
+            var items = {time, date, status, place}
+            list.push(items);
+        });
+    return list;
+    });
+}
+
+app.get('/:provider/:id', async(req, res) => {
+    
+    try{
+        await getDetails(req.params.provider, req.params.id)
+        res.send(list);
+    }
+    catch(err){
+        console.log(err)
+        res.send(err)
+    }
+    
+});
+
+app.get('/', (req, res) => {
+    res.send({url:"INVALID", check_github:"https://github.com/JinsoRaj/AfterShip-Tracking"});
+});
+     
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
+
+module.exports = app;
